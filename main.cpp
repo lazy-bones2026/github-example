5514d806bfc78a14d4266b125ae25a305adf63a6
nmk26730031 <26730031@ms.uit.edu.vn>
2026-09-18 20:38:24 +0700

    Feat: Add removeLine() function

--- main.cpp
+++ main.cpp
@@ -108,8 +108,21 @@ void draw(){
     for (int i = 0 ; i < H ; i++, cout<<endl)
         for (int j = 0 ; j < W ; j++) cout<<board[i][j];
 }
-void removeLine(){
 
+void removeLine(){
+    int i,j;
+    for (i = H-2 ; i > 0 ; i-- ){
+        for (j = 0 ; j < W ; j++)
+            if (board[i][j] == ' ') break;
+        if (j == W){
+            for (int ii = i ; ii > 0 ; ii--)
+                for (int jj = 0; jj < W; jj++)
+                    board[ii][jj] = board[ii-1][jj];
+            i++;
+            draw();
+            _sleep(200);
+        }
+    }
 }
 
 int main()
